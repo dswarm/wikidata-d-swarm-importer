a0038ab94d15560daa8b1e16e7b3a658b3f01a36
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-08-06 17:53:16 +0200

    [WIP] switched to own mediawiki api client: prepared first request (incl. authentication calls (edit token generation needs to be composed))

--- src/main/java/org/dswarm/wikidataimporter/WikibaseAPIClient.java
+++ src/main/java/org/dswarm/wikidataimporter/WikibaseAPIClient.java
@@ -2,24 +2,33 @@ package org.dswarm.wikidataimporter;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.io.Resources;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.rx.RxInvocationBuilder;
 import org.glassfish.jersey.client.rx.RxWebTarget;
 import org.glassfish.jersey.client.rx.rxjava.RxObservable;
 import org.glassfish.jersey.client.rx.rxjava.RxObservableInvoker;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,30 +78,227 @@ public class WikibaseAPIClient {
 			.property(ClientProperties.CONNECT_TIMEOUT, REQUEST_TIMEOUT)
 			.property(ClientProperties.READ_TIMEOUT, REQUEST_TIMEOUT);
 
-	private static final String WIKIBASE_API_ACTION_IDENTIFIER = "action";
-	private static final String WIKIBASE_API_NEW_IDENTIFIER    = "new";
-	private static final String WIKIBASE_API_DATA_IDENTIFIER   = "data";
+	private static final String MEDIAWIKI_API_ACTION_IDENTIFIER = "action";
 
-	private static final String WIKIBASE_API_ITEM_IDENTIFIER = "item";
+	private static final String MEDIAWIKI_API_FORMAT_IDENTIFIER = "format";
+
+	private static final String MEDIAWIKI_API_LOGIN                 = "login";
+	private static final String MEDIAWIKI_API_LGNAME_IDENTIFIER     = "lgname";
+	private static final String MEDIAWIKI_API_LGPASSWORD_IDENTIFIER = "lgpassword";
+	private static final String MEDIAWIKI_API_LGTOKEN_IDENTIFIER    = "lgtoken";
+
+	private static final String MEDIAWIKI_API_QUERY               = "query";
+	private static final String MEDIAWIKI_API_META_IDENTIFIER     = "meta";
+	private static final String MEDIAWIKI_API_CONTINUE_IDENTIFIER = "continue";
+
+	private static final String WIKIBASE_API_NEW_IDENTIFIER  = "new";
+	private static final String WIKIBASE_API_DATA_IDENTIFIER = "data";
+
+	private static final String MEDIAWIKI_API_TOKEN_IDENTIFIER = "token";
+
+	private static final String MEDIAWIKI_API_JSON_FORMAT          = "json";
+	private static final String MEDIAWIKI_API_TOKENS_IDENTIFIER    = "tokens";
+	private static final String MEDIAWIKI_API_CSRFTOKEN_IDENTIFIER = "csrftoken";
+	private static final String WIKIBASE_API_ITEM_IDENTIFIER       = "item";
 
 	private static final String WIKIBASE_API_EDIT_ENTITY = "wbeditentity";
 
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 
-	public static Observable<Response> createEntity(final EntityDocument entity) throws JsonProcessingException {
+	private final String editToken;
 
-		final String entityJSONString = MAPPER.writeValueAsString(entity);
+	public WikibaseAPIClient() {
+
+		editToken = generateEditToken();
+	}
+
+	private String generateEditToken() {
+
+		// 0. read user name + password from properties
+		// 1. login request
+		// 1.1 get token from login request response
+		// 1.2 get cookies from login request response
+		// 2. confirm login request
+		// 2.1 get cookies from login confirm response
+		// 3. retrieve edit token request
+		// 3.1 get edit token from edit token response
+		// 3.2 get cookies from edit token response
+		// 3.3 merge cookies from response from 2 + 3
+
+		// return edit token + cookies
+
+		return null;
+	}
+
+	public static Observable<Response> login(final String username, final String password) {
 
 		final RxWebTarget<RxObservableInvoker> rxWebTarget = rxWebTarget();
 
-		// TODO: which accept media type? - add to request method
-		final RxObservableInvoker rx = rxWebTarget.queryParam(WIKIBASE_API_ACTION_IDENTIFIER, WIKIBASE_API_EDIT_ENTITY)
-				.queryParam(WIKIBASE_API_NEW_IDENTIFIER, WIKIBASE_API_ITEM_IDENTIFIER)
-				.queryParam(WIKIBASE_API_DATA_IDENTIFIER, entityJSONString)
-				.request()
-				.header(HttpHeaders.USER_AGENT, DSWARM_USER_AGENT_IDENTIFIER).rx();
+		final RxObservableInvoker rx = rxWebTarget.request()
+				.header(HttpHeaders.USER_AGENT, DSWARM_USER_AGENT_IDENTIFIER)
+				.rx();
 
-		final Observable<Response> post = rx.post(entity).subscribeOn(Schedulers.from(EXECUTOR_SERVICE));
+		final FormDataMultiPart form = new FormDataMultiPart()
+				.field(MEDIAWIKI_API_ACTION_IDENTIFIER, MEDIAWIKI_API_LOGIN)
+				.field(MEDIAWIKI_API_LGNAME_IDENTIFIER, username)
+				.field(MEDIAWIKI_API_LGPASSWORD_IDENTIFIER, password)
+				.field(MEDIAWIKI_API_FORMAT_IDENTIFIER, MEDIAWIKI_API_JSON_FORMAT);
+
+		return excutePOST(rx, form);
+	}
+
+	public static Observable<Response> confirmLogin(final String token, final Map<String, NewCookie> cookies) {
+
+		final RxObservableInvoker rx = buildBaseRequestWithCookies(cookies);
+
+		final FormDataMultiPart form = new FormDataMultiPart()
+				.field(MEDIAWIKI_API_ACTION_IDENTIFIER, MEDIAWIKI_API_LOGIN)
+				.field(MEDIAWIKI_API_LGTOKEN_IDENTIFIER, token);
+
+		return excutePOST(rx, form);
+	}
+
+	public static Observable<Response> retrieveEditToken(final Map<String, NewCookie> cookies) {
+
+		final RxObservableInvoker rx = buildBaseRequestWithCookies(cookies);
+
+		final FormDataMultiPart form = new FormDataMultiPart()
+				.field(MEDIAWIKI_API_ACTION_IDENTIFIER, MEDIAWIKI_API_QUERY)
+				.field(MEDIAWIKI_API_META_IDENTIFIER, MEDIAWIKI_API_TOKENS_IDENTIFIER)
+				.field(MEDIAWIKI_API_CONTINUE_IDENTIFIER, "");
+
+		return excutePOST(rx, form);
+	}
+
+	public static Observable<String> getToken(final Observable<Response> loginResponse) {
+
+		return loginResponse.map(response1 -> {
+
+			try {
+
+				final String responseBody = response1.readEntity(String.class);
+
+				// TODO null check
+
+				final ObjectNode json = MAPPER.readValue(responseBody, ObjectNode.class);
+
+				// TODO null check
+
+				final JsonNode loginNode = json.get(MEDIAWIKI_API_LOGIN);
+
+				// TODO null check
+
+				final JsonNode tokenNode = loginNode.get(MEDIAWIKI_API_TOKEN_IDENTIFIER);
+
+				// TODO null check
+
+				return tokenNode.asText();
+			} catch (final Exception e) {
+
+				e.printStackTrace();
+
+				// TODO wrap/delegate error
+
+				return null;
+			}
+		});
+	}
+
+	public static Observable<String> getEditToken(final Observable<Response> editTokenResponse) {
+
+		return editTokenResponse.map(response -> {
+
+			try {
+
+				final String responseBody = response.readEntity(String.class);
+
+				// TODO null check
+
+				final ObjectNode json = MAPPER.readValue(responseBody, ObjectNode.class);
+
+				// TODO null check
+
+				final JsonNode queryNode = json.get(MEDIAWIKI_API_QUERY);
+
+				// TODO null check
+
+				final JsonNode tokensNode = queryNode.get(MEDIAWIKI_API_TOKENS_IDENTIFIER);
+
+				// TODO null check
+
+				final JsonNode csrfTokenNode = tokensNode.get(MEDIAWIKI_API_CSRFTOKEN_IDENTIFIER);
+
+				// TODO null check
+
+				return csrfTokenNode.asText();
+			} catch (final Exception e) {
+
+				e.printStackTrace();
+
+				// TODO wrap/delegate error
+
+				return null;
+			}
+		});
+	}
+
+	public static Observable<Response> createEntity(final EntityDocument entity, final String token, final Map<String, NewCookie> cookies)
+			throws JsonProcessingException {
+
+		final String entityJSONString = MAPPER.writeValueAsString(entity);
+
+		final RxObservableInvoker rx = buildBaseRequestWithCookies(cookies);
+
+		final FormDataMultiPart form = new FormDataMultiPart()
+				.field(MEDIAWIKI_API_ACTION_IDENTIFIER, WIKIBASE_API_EDIT_ENTITY)
+				.field(WIKIBASE_API_NEW_IDENTIFIER, WIKIBASE_API_ITEM_IDENTIFIER)
+				.field(WIKIBASE_API_DATA_IDENTIFIER, entityJSONString)
+				.field(MEDIAWIKI_API_TOKEN_IDENTIFIER, token);
+		//form.bodyPart(entityJSONString, MediaType.APPLICATION_JSON_TYPE);
+
+		return excutePOST(rx, form);
+	}
+
+	public static Observable<Map<String, NewCookie>> getCookie(final Observable<Response> response) {
+
+		return response.map(Response::getCookies);
+	}
+
+	private static RxObservableInvoker buildBaseRequestWithCookies(final Map<String, NewCookie> cookies) {
+
+		final RxWebTarget<RxObservableInvoker> rxWebTarget = rxWebTarget();
+
+		RxInvocationBuilder<RxObservableInvoker> request = rxWebTarget.request()
+				.header(HttpHeaders.USER_AGENT, DSWARM_USER_AGENT_IDENTIFIER);
+
+		if (cookies != null) {
+
+			for (final Cookie cookie : cookies.values()) {
+
+				request = request.cookie(cookie);
+			}
+		}
+
+		return request.rx();
+	}
+
+	private static Observable<Response> excutePOST(final RxObservableInvoker rx, final FormDataMultiPart form) {
+
+		final Entity entityBody = Entity.entity(form, MediaType.MULTIPART_FORM_DATA);
+
+		final Observable<Response> post = rx.post(entityBody).subscribeOn(Schedulers.from(EXECUTOR_SERVICE));
+
+		return post.filter(response -> {
+
+			if (response == null) {
+
+				return false;
+			}
+
+			final int status = response.getStatus();
+
+			return status == 200;
+		});
 	}
 
 	private static Client client() {
